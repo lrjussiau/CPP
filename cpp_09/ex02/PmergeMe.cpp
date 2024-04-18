db927cd07e99b4b1ceed1e5afbc3592f57598a01
lrjussiau <lr.jussiaume@gmail.com>
2024-04-18 16:55:24 +0200

    added Ford Johnson explanation

---
+++ cpp_09/ex02/PmergeMe.cpp
@@ -0,0 +1,20 @@
+
+/*
+Ford Johnson
+
+3 step:
+
+1 - Divide the list half and half
+	- Preaty easy 2 container temp
+	- Merge on the final container ??
+
+2 - Sort Each half
+	- Continue to subdivide the list recursivelly until have only 1 or 2 item in each list
+	- Sorting them 
+
+3 - Merge with strategy
+	- Merge them efficiently with binary insertion
+		- find wich part of the list to search etc..until find the right position
+		- Merge all the small part each other until every part is united.
+
+*/
